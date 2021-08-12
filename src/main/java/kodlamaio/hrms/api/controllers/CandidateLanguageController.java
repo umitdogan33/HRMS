26db2d4012932c83a09ede8dac1f278acafdc6f8
umitdogan33 <umit9551@gmail.com>
2021-08-12 13:00:34 +0300

    Refactor

--- src/main/java/kodlamaio/hrms/api/controllers/CandidateLanguageController.java
+++ src/main/java/kodlamaio/hrms/api/controllers/CandidateLanguageController.java
@@ -6,15 +6,13 @@ import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.CandidateLanguage;
 import kodlamaio.hrms.entities.dtos.LanguageDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/candidatelanguage")
+@CrossOrigin
 public class CandidateLanguageController {
     private CandidateLanguageService candidateLanguageService;
 
